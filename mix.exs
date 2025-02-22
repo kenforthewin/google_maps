ecc24dc18708d18b33c0b0737c24e253d77f5f3c
Kenny Bergquist <bergquist.kenneth@gmail.com>
2025-02-21 22:00:07 -0500

    bump

--- mix.exs
+++ mix.exs
@@ -43,7 +43,7 @@ defmodule GoogleMaps.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:httpoison, ">= 1.7"},
+      {:httpoison, ">= 1.7.0"},
       {:jason, "~> 1.1"},
       {:ex_doc, ">= 0.0.0", only: :dev}
     ]
